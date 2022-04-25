784c95e591bf4b2a6939c8a5bd603c6eec956e2d
Shane Park <shanejpark@gmail.com>
2022-04-25 19:05:21 -0400

    added table to drugs

--- server/db.config.js
+++ server/db.config.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 // create here mysql connection
 const dbConn = mysql.createConnection({
   host: "localhost",
--- src/components/Drug.tsx
+++ src/components/Drug.tsx
@@ -14,6 +14,18 @@ import axios from "axios";
 function Drug() {
   const [drugName, setDrugName] = useState("");
   const [manufacturers, setManufacturers] = useState([]);
+  const [drugs, setDrugs] = useState([]);
+
+  async function loadDrugs() {
+    return fetch("http://localhost:5000/api/v1/drugs")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setDrugs(myJson);
+      });
+  }
+
   async function searchManufacturer() {
     return axios
       .get(`http://localhost:5000/api/v1/drugs/manufacturer/${drugName}`)
@@ -21,6 +33,38 @@ function Drug() {
         setManufacturers(response.data);
       });
   }
+
+  function listAllDrugs() {
+    return (
+      <div>
+        <h1>List of Drugs</h1>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Common Name</th>
+              <th>Medical Name</th>
+              <th>Formula</th>
+              <th>Generic</th>
+            </tr>
+          </thead>
+          <tbody>
+            {drugs.map((drug) => (
+              <tr>
+                <td>{drug["common_name"]}</td>
+                <td>{drug["medical_name"]}</td>
+                <td>{drug["formula"]}</td>
+                <td>{drug["generic"]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    loadDrugs();
+  }, []);
 
   function manufacturerTable() {
     return (
@@ -46,6 +90,7 @@ function Drug() {
   return (
     <div className={styles.filter}>
       <Container className={styles.containerBlock}>
+        <Row className="m-5">{listAllDrugs()}</Row>
         <Col>
           <Card className={styles.card}>
             <Card.Body>
